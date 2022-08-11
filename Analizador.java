c9101edd7acd0874236b8317722ceb31f2bdf826
Elio <81540433+Elio-Nicolas@users.noreply.github.com>
2022-08-11 20:06:16 -0300

    Add files via upload

--- Analizador.java
+++ Analizador.java
@@ -10,18 +10,19 @@ package Principal;
  * @author baigo
  */
 import java.util.*;
+import java.io.File;
 
 public class Analizador {
     public static void main(String[] args) {
         
-       // String ruta="C:/Users/baigo/OneDrive/Documentos/NetBeansProjects/Analizador/src/Principal/Lexemas.flex"
-        //generaLexema(ruta);    
+        String ruta="C:/Users/baigo/OneDrive/Documentos/NetBeansProjects/Analizador/src/Principal/Lexemas.flex"
+        generaLexema(ruta);    
         
-        System.out.println("elio");
+        
     }
     
-   /* public static void generaLexema(String ruta){
+    public static void generaLexema(String ruta){
         File archivo= new File(ruta);
         jflex.Main.generate(archivo);
-    }*/
+    }
 }
