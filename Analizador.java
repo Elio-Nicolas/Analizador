7533018227308f4a59d258a4ea30bc42c8a74977
Elio <81540433+Elio-Nicolas@users.noreply.github.com>
2022-08-11 19:34:57 -0300

    se agrega la primera codificacion

---
+++ Analizador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author baigo
+ */
+import java.util.*;
+
+public class Analizador {
+    public static void main(String[] args) {
+        
+       // String ruta="C:/Users/baigo/OneDrive/Documentos/NetBeansProjects/Analizador/src/Principal/Lexemas.flex"
+        //generaLexema(ruta);    
+        
+        System.out.println("elio");
+    }
+    
+   /* public static void generaLexema(String ruta){
+        File archivo= new File(ruta);
+        jflex.Main.generate(archivo);
+    }*/
+}
---
+++ Lexemas.flex
@@ -0,0 +1,26 @@
+Package Principal;
+import static Principal.Tokens.*;
+%%
+%class Lexemas
+%type Tokens
+L=[a-zA-Z_]+
+D=[0-9]+
+espacio=[ ,\t,\r,\n]+
+%{
+    public String lexemas;
+%}
+%%
+int |
+if |
+else |
+while {lexemas=yytext(); return Reservadas;}
+{espacio} {/*Ignorar*/}
+"//".* {/*Ignorar*/}
+"=" {lexemas=yytext();return Igual;}
+"+" {lexemas=yytext();return Suma;}
+"-" {lexemas=yytext();return Resta;}
+"*" {lexemas=yytext();return Multiplicacion;}
+"/" {lexemas=yytext();return Division;}
+{L}({L}|{D})* {lexemas=yytext(); return Identificador;}
+("(-"{D}+")")|{D}+ {lexema=yytext(); return Número;}
+ . {return ERROR;}
